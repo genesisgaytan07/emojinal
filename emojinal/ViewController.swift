2acad34287d98fdbd2cb364cc626034885d32213
Apple <apple@Apples-MacBook-Pro-19.local>
2019-07-12 11:29:05 -0500

    iteration2

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -9,13 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let emojis = ["🥠": "FORTUNE" , "🎲": "DICE"]
+    let showMessages = [
+        "FORTUNE" : [" you will find a great treasure" , "you will discover a new adventure" , "You will meet someone who will bring you happiness"],
+        "DICE" : [ 1 , 2, 3, 4, 5, 6]]
+
     @IBAction func showMessage(sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
+
         // pop up message of the fortune and lucky number of the day
-        let alertController = UIAlertController(title: "COOKIE OR DICE", message: "here....", preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: "COOKIE OR DICE", message: "", preferredStyle: UIAlertController.Style.alert)
          alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         
         present(alertController, animated: true, completion: nil)
+    
     }
+    
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
